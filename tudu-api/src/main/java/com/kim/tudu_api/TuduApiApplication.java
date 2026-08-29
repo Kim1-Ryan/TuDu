f9585ba630eefe514775b2afe29c91e7c550a042
Shockwave <mpramlall@gmail.com>
2026-08-29 17:12:24 +0200

    redundant public

--- tudu-api/src/main/java/com/kim/tudu_api/TuduApiApplication.java
+++ tudu-api/src/main/java/com/kim/tudu_api/TuduApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TuduApiApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(TuduApiApplication.class, args);
 	}
 
